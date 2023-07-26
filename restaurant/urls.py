eaa1445718c86d0991c991b8ff47c3adfbead851
Bohdan Marko <goldenbohdanm@gmail.com>
2023-07-26 21:08:59 +0300

    Create a view for model Dish Type (ListView, Create/Update/DeleteView) + HTML templates for views

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,35 @@
 from django.urls import path
 
-from restaurant.views import index
-
+from restaurant.views import (
+    index,
+    DishTypeListView,
+    DishTypeCreateView,
+    DishTypeUpdateView,
+    DishTypeDeleteView
+)
 
 app_name = "restaurant"
 
 urlpatterns = [
     path("", index, name="index"),
+    path(
+        "dish-types/",
+        DishTypeListView.as_view(),
+        name="dish-type-list"
+    ),
+    path(
+        "dish-types/create/",
+        DishTypeCreateView.as_view(),
+        name="dish-type-create"
+    ),
+    path(
+        "dish-types/<int:pk>/update/",
+        DishTypeUpdateView.as_view(),
+        name="dish-type-update"
+    ),
+    path(
+        "dish-types/<int:pk>/delete/",
+        DishTypeDeleteView.as_view(),
+        name="dish-type-delete"
+    )
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
 from restaurant.models import Dish, Cook, Ingredient, DishType
 
@@ -24,3 +27,31 @@ def index(request):
         "restaurant/index.html",
         context=context
     )
+
+
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
+    model = DishType
+    template_name = "restaurant/dish_type_list.html"
+    context_object_name = "dish_type_list"
+    paginate_by = 5
+
+
+class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
+    model = DishType
+    fields = "__all__"
+    template_name = "restaurant/dish_type_form.html"
+    success_url = reverse_lazy("restaurant:dish-type-list")
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    template_name = "restaurant/dish_type_form.html"
+    success_url = reverse_lazy("restaurant:dish-type-list")
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    fields = "__all__"
+    template_name = "restaurant/dish_type_delete.html"
+    success_url = reverse_lazy("restaurant:dish-type-list")
