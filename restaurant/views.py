11e949af5645d6c39c6ab9c2e1d62eab148a715f
Bohdan Marko <goldenbohdanm@gmail.com>
2023-09-10 16:27:30 +0300

    Update a design for index (main) page.

--- restaurant/views.py
+++ restaurant/views.py
@@ -12,13 +12,11 @@ from restaurant.models import Dish, Cook, Ingredient, DishType
 def index(request):
     num_dishes = Dish.objects.count()
     num_cooks = Cook.objects.count()
-    num_dishtype = DishType.objects.count()
     num_ingredients = Ingredient.objects.count()
 
     context = {
         "num_dishes": num_dishes,
         "num_cooks": num_cooks,
-        "num_dishtype": num_dishtype,
         "num_ingredients": num_ingredients
     }
 
